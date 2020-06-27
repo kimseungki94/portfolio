355e6b0e6ab10f1fd53b7d0158821759157f045b
김승기 <skkim0832@naver.com>
2020-06-27 11:18:12 +0900

    push

--- practice/js부분/hositing.js
+++ practice/js부분/hositing.js
@@ -1,6 +1,13 @@
 sayHi(); // 'hi'
-sayHello() ; // what?? 그런 함수 없어
+
+//sayHello() ; // what?? 그런 함수 없어
+console.log(hi);
+console.log(sayHello);
 
 var hi ='hi';
-function sayHi(){ console.log('hi') }
+function sayHi(){ 
+    console.log('hi콘솔') 
+    var i=3;
+    console.log(i);
+}
 var sayHello = function(){ console.log('hello') };
