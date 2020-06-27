722e75a5b90e99b672cdbed4d2de9651e22d9e51
김승기 <skkim0832@naver.com>
2020-06-27 14:32:58 +0900

    cors test

--- practice/js부분/ajax_test.js
+++ practice/js부분/ajax_test.js
@@ -4,5 +4,6 @@ oReq.addEventListener("load", function() {
 });    
 oReq.open("GET", "./station.txt");// 브라우져로 실행시 cors 문제 생김.. File형식을 못받아서 그런듯
 //http로 해야 헤더형식으로 가져오는 cors에 맞기 때문에!
+
 oReq.send();
 
--- practice/js부분/cors.js
+++ practice/js부분/cors.js
@@ -1,40 +1,41 @@
-(function() {
-    var httpRequest;
-    document.getElementById("ajaxButton").onclick = function() { makeRequest(); };
-   
-    function makeRequest() {
-      if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        httpRequest = new XMLHttpRequest();
-      } else if (window.ActiveXObject) { // IE
-        try {
-          httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-        } 
-        catch (e) {
-          try {
-            httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-          } 
-          catch (e) {}
-        }
+    // Create the XHR object.
+    function createCORSRequest(method, url) {
+      var xhr = new XMLHttpRequest();
+      if ("withCredentials" in xhr) {
+          // XHR for Chrome/Firefox/Opera/Safari.
+          xhr.open(method, url, true);
+      } else if (typeof XDomainRequest != "undefined") {
+          // XDomainRequest for IE.
+          xhr = new XDomainRequest();
+          xhr.open(method, url);
+      } else {
+          // CORS not supported.
+          xhr = null;
       }
-   
-      if (!httpRequest) {
-        alert('Giving up :( Cannot create an XMLHTTP instance');
-        return false;
+      return xhr;
+  }
+
+
+  // Make the actual CORS request.
+  function makeCorsRequest() {
+      // All HTML5 Rocks properties support CORS.
+      var url = 'http://callback.oblax.ru/catalog/view/javascript/widget/view/widget/index.html';
+
+      var xhr = createCORSRequest('post', url);
+      if (!xhr) {
+          alert('CORS not supported');
+          return;
       }
-      httpRequest.onreadystatechange = alertContents;
-      httpRequest.open('GET', "./station.txt");
-      httpRequest.send();
-    }
-   
-    function alertContents() {
-      if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-          alert(httpRequest.responseText);
-          console.log(httpRequest.responseText);
-          
-        } else {
-          alert('There was a problem with the request.');
-        }
-      }
-    }
-  })();
+
+      // Response handlers.
+      xhr.onload = function () {
+          alert('Response from CORS request to ' + url + ': ');
+      };
+
+      xhr.onerror = function () {
+          alert('Woops, there was an error making the request.');
+      };
+
+      xhr.send();
+  }
+  makeCorsRequest();
