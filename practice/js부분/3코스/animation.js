c3ef7350cb624c142affd9d29213f250d295238e
김승기 <skkim0832@naver.com>
2020-07-22 01:18:11 +0900

    애니매이션 기능 테스트

---
+++ practice/js부분/3코스/animation.js
@@ -0,0 +1,9 @@
+// var base = document.querySelector(".outside");
+// base.style.transform = "scale(4)";
+// base.style.left = "500px";
+var btn = document.querySelector("button");
+var target = document.querySelector(".outside");
+btn.addEventListener("click", function () {
+  var pre = parseInt(target.style.left);
+  target.style.left = pre + 100 + "px";
+});
