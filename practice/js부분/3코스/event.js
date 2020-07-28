d5f884867009196b67e976f959ff4b53c401213a
김승기 <skkim0832@naver.com>
2020-07-28 20:24:38 +0900

    dom을 통한 동적처리 예제

--- practice/js부분/3코스/event.js
+++ practice/js부분/3코스/event.js
@@ -6,10 +6,21 @@ var img = document.querySelector("img");
 //     log.innerHTML = "clicked" + evt.currentTarget.firstElementChild.src;
 //   });
 // }
-img.addEventListener("click", function () {
-  console.log("img tag clicked");
-});
+// ul.addEventListener("click", function (evt) {
+//   //IMG UL
+//   console.log(evt.target.tagName, evt.currentTarget.tagName);
+// });
+// img.addEventListener("click", function () {
+//   console.log("image clicked");
+// });
+
+// target 각각의 태그의 특징을 호출하는 기능!
+var ul = document.querySelector("ul");
 ul.addEventListener("click", function (evt) {
-  //IMG UL
-  console.log(evt.target.tagName, evt.currentTarget.tagName);
+  debugger;
+  if (evt.target.tagName === "IMG") {
+    log.innerHTML = "clicked" + evt.target.src;
+  } else if (evt.target.tagName === "LI") {
+    log.innerHTML = "clicked" + evt.target.firstChild.src;
+  }
 });
